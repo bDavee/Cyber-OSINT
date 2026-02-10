956c8e8e8460f73cf499a6ed6031ad2ce194cbce
Bankuti-oliver <bankutiolivern@elg-bp.edu.hu>
2026-02-10 10:11:53 +0100

    Datebase & Tables done

---
+++ Banyaszati_teruletek/main.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE mbt;
+
+
+
+CREATE TABLE telek (
+    id int NOT NULL AUTO_INCREMENT,
+    telepules varchar(200),
+    muvmod varchar(200),
+    allapot varchar(1),
+    fedoszint decimal(6,2),
+    fekuszint decimal(6,2),
+    PRIMARY KEY (id)  
+);
+
+CREATE TABLE kapcsolo(
+    telek_id int NOT NULL,
+    nyersanyagid int NOT NULL,
+    PRIMARY KEY (nyersanyagid),
+   
+
+);
+
+CREATE TABLE nyersanyag (
+   id int NOT NULL,
+   nev varchar(250),
+   PRIMARY KEY (id)
+
+
+);

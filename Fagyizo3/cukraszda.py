486e2844b15a4361dd78284e529f1c889eff0332
Kiskornsz Maxim <kiskornszmaxim@EOTVOS2.LOCAL>
2026-02-10 10:05:47 +0100

    class elkeszitese

--- Fagyizo3/cukraszda.py
+++ Fagyizo3/cukraszda.py
@@ -1,2 +1,11 @@
 import os,random
 os.system("cls")
+
+class Suti:
+    def __init__(self, nev:str, tipus:str, ar:int, egyseg:str):
+        self.nev = nev
+        self.tipus = tipus
+        self.ar = ar
+        self.egyseg = egyseg
+        self.eladas = 0
+        self.bevetel = 0

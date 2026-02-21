a2676bd4bf4fc51b2f6ac75b7d56a7d7c61e128b
Bankuti-oliver <bankutiolivern@elg-bp.edu.hu>
2026-02-21 17:19:43 +0100

    login basic added

--- Csapatweb/main.js
+++ Csapatweb/main.js
@@ -1,14 +1,12 @@
-
 $.ajax({
-    url: 'https://ceg.atanarur.hu/api.php',
-    type: 'POST',
-    data: {
-        username: localStorage.getItem("username"),
-        hash: localStorage.getItem("hash"),
-        password: 'randomszoveg',
-        type: 'team'
-    },
-    success: function (result) {
-        console.log(result);
-    }
+ url:'ceg.atanarur.hu/api.php',
+ type: 'POST',
+ data: {
+    username: 'Felhasználónév',
+    password: 'JeLsZó',
+    type: 'login'
+},
+success: function (result) {
+console.log(result);
+}
 });

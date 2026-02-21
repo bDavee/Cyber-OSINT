7830b26b3b9c8a14995fd9a66f30b50327ba9422
Bankuti-oliver <bankutiolivern@elg-bp.edu.hu>
2026-02-21 17:34:31 +0100

    js form consts added

--- Csapatweb/main.js
+++ Csapatweb/main.js
@@ -1,12 +1,20 @@
-$.ajax({
- url:'ceg.atanarur.hu/api.php',
- type: 'POST',
- data: {
-    username: 'Felhasználónév',
-    password: 'JeLsZó',
-    type: 'login'
-},
-success: function (result) {
-console.log(result);
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const form_button = document.getElementById("login_button");
+
+    const form = document.querySelector("form");
+    const form_username = document.getElementById("login_username");
+    const form_password = document.getElementById("login_password");
+
+    $.ajax({
+        url: 'ceg.atanarur.hu/api.php',
+        type: 'POST',
+        data: {
+            username: 'Felhasználónév',
+            password: 'JeLsZó',
+            type: 'login'
+        },
+        success: function (result) {
+            console.log(result);
+        }
+    });
 });

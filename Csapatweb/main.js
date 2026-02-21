8bc13b5e004a2411607536109922f665da9a72ad
Bankuti-oliver <bankutiolivern@elg-bp.edu.hu>
2026-02-21 18:00:20 +0100

    setcookie and result is WIP

--- Csapatweb/main.js
+++ Csapatweb/main.js
@@ -5,16 +5,40 @@ document.addEventListener("DOMContentLoaded", () => {
     const form_username = document.getElementById("login_username");
     const form_password = document.getElementById("login_password");
 
-    $.ajax({
-        url: 'ceg.atanarur.hu/api.php',
-        type: 'POST',
-        data: {
-            username: 'Felhasználónév',
-            password: 'JeLsZó',
-            type: 'login'
-        },
-        success: function (result) {
-            console.log(result);
-        }
+
+    form_button.addEventListener("click", function (send) {
+
+        send.preventDefault(); 
+
+        $.ajax({
+            url: 'https://ceg.atanarur.hu/api.php',
+            type: 'POST',
+            data: {
+                username: form_username.value,
+                password: form_password.value,
+                type: 'login'
+            },
+            success: function (result) {
+                console.log(result);
+            let data = JSON.parse(result);
+        if (data.status === "success") {
+             setCookie("hash", data.hash, 1);}
+        else {
+                    console.log("Error:", data.message);
+                }
+            }
+        });
+
     });
+   
+
+
+    function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+     d.setTime(d.getTime() + (exdays*24*60*60*1000));
+      let expires = "expires="+ d.toUTCString();
+     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+    
+    
 });
